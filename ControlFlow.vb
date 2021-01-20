fc5d012fb59411dbfefe65ddf8f00f8bf1bda852
Tim Rossiter <48777447+rosstimo@users.noreply.github.com>
2021-01-20 08:53:28 -0700

    add if example, add console read line example

--- ControlFlow.vb
+++ ControlFlow.vb
@@ -1,6 +1,30 @@
-﻿Module ControlFlow
+﻿Option Explicit On
+Option Strict On
+Option Compare Binary
+
+Module ControlFlow
 
     Sub Main()
+        'Dim firstNumber As Decimal
+        'Dim secondNumber As Integer
+        'firstNumber = 10
+        'secondNumber = 8
+        'If firstNumber > secondNumber Then
+        '    Console.WriteLine("First Number is Bigger!!")
+        'ElseIf firstNumber < secondNumber Then
+        '    Console.WriteLine("Second Number is Bigger!!")
+        'Else
+        '    Console.WriteLine("The Numbers Are Equal!!")
+        'End If
+
+        Dim userInput As String
+        Console.WriteLine("Type a word and press Enter")
+        userInput = Console.ReadLine()
+
+        'Console.WriteLine("you typed " & userInput)
+        Console.WriteLine($"you typed {userInput}")
+
+        Console.Read()
 
     End Sub
 
