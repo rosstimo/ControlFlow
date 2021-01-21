8c4929fad6418452f02ad41fb11aacbe4191562d
Tim Rossiter <48777447+rosstimo@users.noreply.github.com>
2021-01-21 08:45:04 -0700

    add conversion and try catch example

--- ControlFlow.vb
+++ ControlFlow.vb
@@ -17,12 +17,53 @@ Module ControlFlow
         '    Console.WriteLine("The Numbers Are Equal!!")
         'End If
 
-        Dim userInput As String
-        Console.WriteLine("Type a word and press Enter")
-        userInput = Console.ReadLine()
+
+        'Dim userInput As String
+        'Console.WriteLine("Type a word and press Enter")
+        'userInput = Console.ReadLine()
 
         'Console.WriteLine("you typed " & userInput)
-        Console.WriteLine($"you typed {userInput}")
+        'Console.WriteLine($"you typed {userInput}")
+
+
+
+
+        'Dim firstNumber As String
+        'Dim secondNumber As String
+        'Console.WriteLine(CInt(firstNumber) + CInt(secondNumber))
+
+        Dim firstNumber As Integer
+        Dim secondNumber As Integer
+        Dim userInput As String
+        Dim problem As Boolean = False
+
+        Console.WriteLine("enter a number")
+        Try
+            userInput = Console.ReadLine()
+            firstNumber = CInt(userInput)
+        Catch e As Exception
+            Console.WriteLine($"I need a number. you entered {userInput}")
+            problem = True
+        End Try
+
+        Console.WriteLine("enter a number")
+        Try
+            userInput = Console.ReadLine()
+            secondNumber = CInt(userInput)
+        Catch e As Exception
+            Console.WriteLine($"I need a number. you entered {userInput}")
+            problem = True
+        End Try
+
+
+        If problem Then
+            Console.WriteLine("Sorry try again later...")
+            Console.WriteLine("Have a nice day...")
+        Else
+            'otherwise do the math and display the result
+            Console.WriteLine($"{firstNumber} + {secondNumber} = {firstNumber + secondNumber}")
+        End If
+
 
         Console.Read()
 
