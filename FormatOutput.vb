e61c135a36b0b3d6533b4c1b7c067d0b64889b78
Tim Rossiter <48777447+rosstimo@users.noreply.github.com>
2021-01-26 08:52:09 -0700

    add column format loop

---
+++ FormatOutput.vb
@@ -0,0 +1,25 @@
+﻿Option Explicit On
+Option Strict On
+
+Module FormatOutput
+
+    Sub main()
+
+        Dim temp As String
+
+        For j = 0 To 10
+            For i = 0 To 15
+                temp = i.ToString("D3")
+                'Console.WriteLine($"The string: {temp.PadLeft(7)} is {Len(temp)} characters long")
+                If i = 5 Then
+                    temp = ""
+                End If
+                Console.Write(temp.PadRight(5))
+            Next
+            Console.WriteLine()
+        Next
+        Console.Read()
+
+    End Sub
+
+End Module
